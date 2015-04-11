1e516b5cec232ea3087f4a183c54fd24ba55b3f9
hbzhang <hbzhang@vt.edu>
2015-04-11 11:11:30 -0400

    change

--- app/controllers/dashing/events_controller.rb
+++ app/controllers/dashing/events_controller.rb
@@ -13,6 +13,7 @@ module Dashing
           response.stream.write("data: #{data}\n\n")
         end
       end
+      @redis.flushall
       @redis.shutdown { |conn| conn.close }
     rescue IOError
       logger.info "[Dashing][#{Time.now.utc.to_s}] Stream closed"
